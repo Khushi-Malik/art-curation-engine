1b4d9f50da81b788e5c38e5f349584e386d4e6d7
Khushi-Malik <khushimalik1910@gmail.com>
2026-01-13 13:01:15 -0500

    fixed css styles and connection

--- art-app/components/HomePage.jsx
+++ art-app/components/HomePage.jsx
@@ -4,6 +4,6 @@ import styles from '../styles/HomePage.module.css'
 
 export default function HomePage() {
   return (
-        <h1 className="title">Welcome to the Art Curation Engine</h1>
+        <h1 className={styles.title}>Welcome to the Art Curation Engine</h1>
   );
 }
